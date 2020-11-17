49ec6937cb4197803f126010dc92c6e335cef7cb
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-11-16 21:11:44 -0600

    composition exercise

---
+++ src/com/practices/CompositionPractices/CarKey.java
@@ -0,0 +1,21 @@
+package com.practices.CompositionPractices;
+
+public class CarKey {
+    private String style;
+    private boolean isRemote;
+
+    public CarKey(String style, boolean isRemote) {
+        this.style = style;
+        this.isRemote = isRemote;
+    }
+    public void start() {
+        System.out.println("Car is starting remotely");
+    }
+    public String getStyle() {
+        return style;
+    }
+
+    public boolean isRemote() {
+        return isRemote;
+    }
+}
