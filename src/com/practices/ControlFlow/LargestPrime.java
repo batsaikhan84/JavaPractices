99abc9eb1cbae5dba200f6f3422cde8583f9ea34
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-11-22 20:32:13 -0600

    LargestPrime

---
+++ src/com/practices/ControlFlow/LargestPrime.java
@@ -0,0 +1,20 @@
+package com.practices.ControlFlow;
+
+public class LargestPrime {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(217));
+
+    }
+    public static int getLargestPrime(int number) {
+        if (number <= 1) {
+            return -1;
+        }
+        for (int i = (number / 2); i > 1; i--) {
+            if (number % i == 0) {
+                number = i;
+            }
+        }
+        return number;
+    }
+}
+
