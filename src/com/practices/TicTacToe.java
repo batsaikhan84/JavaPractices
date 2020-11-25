b6c33571118c45baae3f05ab8cbf7c50abc03bd9
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-11-24 23:41:57 -0600

    able to store user inputs

--- src/com/practices/TicTacToe.java
+++ src/com/practices/TicTacToe.java
@@ -7,21 +7,21 @@ import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Please enter number between 1 - 9");
-//        while (true) {
-//            int userInput = scanner.nextInt();
-//            System.out.println("You entered: " + userInput);
-//        }
-        printingInput(9);
-    }
-    public static void printingInput(int userInput) {
         String[] arrayXO = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
-        for (int i = 0; i < arrayXO.length; i++) {
-            if (userInput - 1 == i) {
-                arrayXO[i] = "x";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter number between 1 - 9");
+        while (true) {
+            int userInput = scanner.nextInt();
+            for (int i = 0; i < arrayXO.length; i++) {
+                if (userInput - 1 == i) {
+                    arrayXO[i] = "x";
+                }
             }
+            printingInput(arrayXO);
         }
+
+    }
+    public static void printingInput(String[] arrayXO) {
         System.out.println("|" + arrayXO[0] + "|" + arrayXO[1] + "|" + arrayXO[2] + "|");
         System.out.println("-------");
         System.out.println("|" + arrayXO[3] + "|" + arrayXO[4] + "|" + arrayXO[5] + "|");
