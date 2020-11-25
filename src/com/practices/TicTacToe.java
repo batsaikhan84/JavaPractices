4ead30381d24579418344db17e236cbe0fd73869
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-11-25 00:01:29 -0600

    players are automatically switching

--- src/com/practices/TicTacToe.java
+++ src/com/practices/TicTacToe.java
@@ -8,12 +8,14 @@ import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args) {
         String[] arrayXO = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
+        String currentPlayer = "x";
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter number between 1 - 9");
         while (true) {
             int userInput = scanner.nextInt();
             if (arrayXO[userInput - 1] == " ") {
-                arrayXO[userInput - 1] = "x";
+                arrayXO[userInput - 1] = currentPlayer;
+                currentPlayer = lastPlayer(currentPlayer);
                 printingInput(arrayXO);
             } else {
                 System.out.println("Please choose different number");
@@ -28,5 +30,11 @@ public class TicTacToe {
         System.out.println("|" + arrayXO[3] + "|" + arrayXO[4] + "|" + arrayXO[5] + "|");
         System.out.println("-------");
         System.out.println("|" + arrayXO[6] + "|" + arrayXO[7] + "|" + arrayXO[8] + "|");
+    }
+    public static String lastPlayer(String currentPlayer) {
+        if (currentPlayer == "x") {
+            return "o";
+        }
+        return "x";
     }
 }
