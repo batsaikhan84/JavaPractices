e2670d01743ea2a1ae6ea64a3304d77d6f66db9b
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-11-24 23:33:49 -0600

    add array containing x o's

--- src/com/practices/TicTacToe.java
+++ src/com/practices/TicTacToe.java
@@ -2,6 +2,7 @@ package com.practices;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -12,10 +13,15 @@ public class TicTacToe {
 //            int userInput = scanner.nextInt();
 //            System.out.println("You entered: " + userInput);
 //        }
-        printingInput();
+        printingInput(9);
     }
-    public static void printingInput() {
+    public static void printingInput(int userInput) {
         String[] arrayXO = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
+        for (int i = 0; i < arrayXO.length; i++) {
+            if (userInput - 1 == i) {
+                arrayXO[i] = "x";
+            }
+        }
         System.out.println("|" + arrayXO[0] + "|" + arrayXO[1] + "|" + arrayXO[2] + "|");
         System.out.println("-------");
         System.out.println("|" + arrayXO[3] + "|" + arrayXO[4] + "|" + arrayXO[5] + "|");
