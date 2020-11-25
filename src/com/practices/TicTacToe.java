c21326cf0893b9314e99606c2fb17b5fa3c2a5cc
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-11-24 23:27:56 -0600

    Implemented displaying x o 's

--- src/com/practices/TicTacToe.java
+++ src/com/practices/TicTacToe.java
@@ -2,13 +2,24 @@ package com.practices;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        System.out.println("|   |   |   |");
-        System.out.println("-------------");
-        System.out.println("|   |   |   |");
-        System.out.println("-------------");
-        System.out.println("|   |   |   |");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter number between 1 - 9");
+//        while (true) {
+//            int userInput = scanner.nextInt();
+//            System.out.println("You entered: " + userInput);
+//        }
+        printingInput();
     }
-
+    public static void printingInput() {
+        String[] arrayXO = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
+        System.out.println("|" + arrayXO[0] + "|" + arrayXO[1] + "|" + arrayXO[2] + "|");
+        System.out.println("-------");
+        System.out.println("|" + arrayXO[3] + "|" + arrayXO[4] + "|" + arrayXO[5] + "|");
+        System.out.println("-------");
+        System.out.println("|" + arrayXO[6] + "|" + arrayXO[7] + "|" + arrayXO[8] + "|");
+    }
 }
