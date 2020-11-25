945cc5c6074915fe9c0aa2b9efbd861323f3944f
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-11-24 20:28:45 -0600

    Tic Tac Toe project initial commit

---
+++ src/com/practices/TicTacToe.java
@@ -0,0 +1,14 @@
+package com.practices;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("|   |   |   |");
+        System.out.println("-------------");
+        System.out.println("|   |   |   |");
+        System.out.println("-------------");
+        System.out.println("|   |   |   |");
+    }
+
+}
