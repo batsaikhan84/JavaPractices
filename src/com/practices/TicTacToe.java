91379a167e3aab8626549883b2013b8eec1f65b7
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-11-26 20:41:03 -0600

    winning combinations

--- src/com/practices/TicTacToe.java
+++ src/com/practices/TicTacToe.java
@@ -7,22 +7,22 @@ import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        String[] arrayXO = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
-        String currentPlayer = "x";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter number between 1 - 9");
-        while (true) {
-            int userInput = scanner.nextInt();
-            if (arrayXO[userInput - 1] == " ") {
-                arrayXO[userInput - 1] = currentPlayer;
-                currentPlayer = lastPlayer(currentPlayer);
-                printingInput(arrayXO);
-            } else {
-                System.out.println("Please choose different number");
-            }
-
-        }
-
+//        String[] arrayXO = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
+//        String currentPlayer = "x";
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter number between 1 - 9");
+//        while (true) {
+//            int userInput = scanner.nextInt();
+//            if (arrayXO[userInput - 1] == " ") {
+//                arrayXO[userInput - 1] = currentPlayer;
+//                currentPlayer = lastPlayer(currentPlayer);
+//                printingInput(arrayXO);
+//            } else {
+//                System.out.println("Please choose different number");
+//            }
+//
+//        }
+        winningCombinations();
     }
     public static void printingInput(String[] arrayXO) {
         System.out.println("|" + arrayXO[0] + "|" + arrayXO[1] + "|" + arrayXO[2] + "|");
@@ -38,6 +38,16 @@ public class TicTacToe {
         return "x";
     }
     public static void win(String[] arrayXO) {
+        for (int i = 0; i < arrayXO.length; i++) {
+        }
+    }
+    public static void winningCombinations() {
+        int[][] winningCombo = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+        for (int i = 0; i < winningCombo.length; i++) {
+            for (int j = 0; j < winningCombo[i].length; j++) {
 
+            }
+            System.out.println(Arrays.toString(winningCombo[i]));
+        }
     }
 }
