4bae51ef3c08aeac481ed0cb7138cf7d0a163321
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-11-28 23:59:44 -0600

    Able to identify winner x or o

--- src/com/practices/TicTacToe.java
+++ src/com/practices/TicTacToe.java
@@ -7,23 +7,27 @@ import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        String[] arrayXO = {"x", "x", "o", "o", "o", " ", " ", " ", " "};
-//        String[] arrayXO = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
-//        String currentPlayer = "x";
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Please enter number between 1 - 9");
-//        while (true) {
-//            int userInput = scanner.nextInt();
-//            if (arrayXO[userInput - 1] == " ") {
-//                arrayXO[userInput - 1] = currentPlayer;
-//                currentPlayer = lastPlayer(currentPlayer);
-//                printingInput(arrayXO);
-//            } else {
-//                System.out.println("Please choose different number");
-//            }
-//
-//        }
-        System.out.println(winningCombinations(arrayXO));
+
+        String[] arrayXO = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
+        String currentPlayer = "x";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter number between 1 - 9");
+        while (true) {
+            int userInput = scanner.nextInt();
+            if (arrayXO[userInput - 1] == " " ) {
+                arrayXO[userInput - 1] = currentPlayer;
+                printingInput(arrayXO);
+                if (win(arrayXO)) {
+                    System.out.println("Player " + currentPlayer + " won!");
+                }
+                currentPlayer = lastPlayer(currentPlayer);
+            } else {
+                System.out.println("Please choose different number");
+            }
+
+        }
+//        String[] arrayXO = {"x", "x", "o", "o", "o", " ", " ", " ", " "};
+//        System.out.println(winningCombinations(arrayXO));
     }
     public static void printingInput(String[] arrayXO) {
         System.out.println("|" + arrayXO[0] + "|" + arrayXO[1] + "|" + arrayXO[2] + "|");
@@ -38,18 +42,13 @@ public class TicTacToe {
         }
         return "x";
     }
-    public static void win(String[] arrayXO) {
-        for (int i = 0; i < arrayXO.length; i++) {
-        }
-    }
-    public static String winningCombinations(String[] arrayXO) {
+    public static boolean win(String[] arrayXO) {
         int[][] winningCombo = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
-        String winner = " ";
         for (int i = 0; i < winningCombo.length; i++) {
             if (arrayXO[winningCombo[i][0]] == arrayXO[winningCombo[i][1]] && arrayXO[winningCombo[i][1]] == arrayXO[winningCombo[i][2]] && arrayXO[winningCombo[i][2]] != " ") {
-                winner = arrayXO[winningCombo[i][0]];
+                return true;
             }
         }
-        return winner;
+        return false;
     }
 }
